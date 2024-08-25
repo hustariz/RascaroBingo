77df42b3eef82f0b1fada9fba5ad033e86a9e878
Hastarus <Hugo.ustariz@gmail.com>
2024-08-25 19:33:25 +0200

    Added BG Image and Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <HelloWorld msg="Welcome to RascaroBingo"/>
-  <img alt="Vue logo" src="./assets/Mini-Kakul.jpg">
+  <div id="app">
+    <Navbar />
+    <HelloWorld msg="Welcome to RascaroBingo"/>
+    <img alt="Vue logo" src="./assets/Mini-Kakul.jpg">
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/AppNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  background-image: url('./assets/Temporary_background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  min-height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
 }
 </style>
