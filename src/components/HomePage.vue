01aafafcc10c2cad83bed94011836995c537f3ae
Hastarus <Hugo.ustariz@gmail.com>
2024-08-27 15:11:11 +0200

    "Start Here" Animation

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="hello">
+  <div class="home-page">
     <h1>{{ welcomeMessage }}</h1>
     <p>
       Rascarobingo is a web application designed to help you manage your emotion and risk management during your trading session, <br> by providing a playful & stimulant frame !
     </p>
-    <img alt="Kakul logo" src="../assets/Mini_Kakul_Transp.png">
+    <div class="logo-container">
+      <router-link to="/bingo"> 
+        <img alt="Kakul logo" class="kakul-img" src="../assets/Mini_Kakul_Transp.png">
+        <span class="tooltip">Start Here</span>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import '../assets/styles/HomePage.css';
+
 export default {
   name: 'HomePage',
   data() {
@@ -17,26 +24,4 @@ export default {
     }
   }
 }
-
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #ff0000;
-}
-.hello {
-  margin-top: 60px; /* This matches the navbar height */
-}
-</style>
