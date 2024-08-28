aa70492fb2403ba634e16f4f058d09ec8fe7b6af
Hastarus <Hugo.ustariz@gmail.com>
2024-08-28 17:56:01 +0200

    Navbar fonts, added some css files

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -11,40 +11,9 @@
 </template>
 
 <script>
+import '../assets/styles/AppNavbar.css';
+
 export default {
   name: 'AppNavbar'
 }
 </script>
-
-<style scoped>
-.navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255,0) 50%, rgba(255,255,255,0));
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1000;
-}
-
-.navbar-brand {
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: red;
-  text-decoration: none; /* Remove underline from link */
-  cursor: pointer; /* Change cursor to pointer on hover */
-}
-
-.navbar-menu { 
-  display: flex;
-}
-
-.navbar-item {
-  margin-left: 1rem;
-  text-decoration: none;
-  color: #ffffff;
-}
-</style>
