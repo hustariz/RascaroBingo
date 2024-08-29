9dc32495d35620b53a6881ecff7d2ce3ad27e4aa
Hastarus <Hugo.ustariz@gmail.com>
2024-08-29 15:00:57 +0200

    Contact form frontend: done

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,16 +4,34 @@
     <router-link to="/" class="navbar-brand">RascaroBingo</router-link>
     <div class="navbar-menu">
       <router-link to="/bingo" class="navbar-item">Bingo</router-link>
-      <a href="#" class="navbar-item">Contact</a>
+      <a href="#" class="navbar-item" @click.prevent="showContactForm">Contact</a>
       <a href="#" class="navbar-item">Login</a>
     </div>
+    <ContactForm :isOpen="isContactFormOpen" @close="closeContactForm" />
   </nav>
 </template>
 
 <script>
 import '../assets/styles/AppNavbar.css';
+import ContactForm from './ContactForm.vue';
 
 export default {
-  name: 'AppNavbar'
+  name: 'AppNavbar',
+  components: {
+    ContactForm
+  },
+  data() {
+    return {
+      isContactFormOpen: false
+    }
+  },
+  methods: {
+    showContactForm() {
+      this.isContactFormOpen = true;
+    },
+    closeContactForm() {
+      this.isContactFormOpen = false;
+    }
+  }
 }
 </script>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,57 @@
+<!-- src/components/ContactForm.vue -->
+<template>
+    <div class="contact-form-overlay" v-if="isOpen" @click.self="closeForm">
+      <div class="contact-form">
+        <h2>Contact Us</h2>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" v-model="name" required>
+          </div>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required>
+          </div>
+          <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea id="message" v-model="message" required></textarea>
+          </div>
+          <div class="form-actions">
+            <button type="submit">Send</button>
+            <button type="button" @click="closeForm">Close</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import '../assets/styles/ContactForm.css';
+
+  export default {
+    name: 'ContactForm',
+    props: {
+      isOpen: {
+        type: Boolean,
+        required: true
+      }
+    },
+    data() {
+      return {
+        name: '',
+        email: '',
+        message: ''
+      }
+    },
+    methods: {
+      submitForm() {
+        // Handle form submission here
+        console.log('Form submitted:', { name: this.name, email: this.email, message: this.message });
+        this.closeForm();
+      },
+      closeForm() {
+        this.$emit('close');
+      }
+    }
+  }
+  </script>
