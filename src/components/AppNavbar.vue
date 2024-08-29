b0af7d3ec9a24d623797a266705a5c39d332716e
Hastarus <Hugo.ustariz@gmail.com>
2024-08-29 15:29:37 +0200

    Login & Register Form + styles

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -5,24 +5,32 @@
     <div class="navbar-menu">
       <router-link to="/bingo" class="navbar-item">Bingo</router-link>
       <a href="#" class="navbar-item" @click.prevent="showContactForm">Contact</a>
-      <a href="#" class="navbar-item">Login</a>
+      <a href="#" class="navbar-item" @click.prevent="showLoginForm">Login</a>
     </div>
     <ContactForm :isOpen="isContactFormOpen" @close="closeContactForm" />
+    <LoginForm :isOpen="isLoginFormOpen" @close="closeLoginForm" @openRegister="showRegisterForm" />
+    <RegisterForm :isOpen="isRegisterFormOpen" @close="closeRegisterForm" />
   </nav>
 </template>
 
 <script>
 import '../assets/styles/AppNavbar.css';
 import ContactForm from './ContactForm.vue';
+import LoginForm from './LoginForm.vue';
+import RegisterForm from './RegisterForm.vue';
 
 export default {
   name: 'AppNavbar',
   components: {
-    ContactForm
+    ContactForm,
+    LoginForm,
+    RegisterForm
   },
   data() {
     return {
-      isContactFormOpen: false
+      isContactFormOpen: false,
+      isLoginFormOpen: false,
+      isRegisterFormOpen: false
     }
   },
   methods: {
@@ -31,6 +39,18 @@ export default {
     },
     closeContactForm() {
       this.isContactFormOpen = false;
+    },
+    showLoginForm() {
+      this.isLoginFormOpen = true;
+    },
+    closeLoginForm() {
+      this.isLoginFormOpen = false;
+    },
+    showRegisterForm() {
+      this.isRegisterFormOpen = true;
+    },
+    closeRegisterForm() {
+      this.isRegisterFormOpen = false;
     }
   }
 }
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,58 @@
+<!-- src/components/LoginForm.vue -->
+<template>
+    <div class="form-overlay" v-if="isOpen" @click.self="closeForm">
+      <div class="form">
+        <h2>Login</h2>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required>
+          </div>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required>
+          </div>
+          <div class="form-actions">
+            <button type="submit">Login</button>
+            <button type="button" @click="closeForm">Close</button>
+          </div>
+        </form>
+        <p class="sign-in-link">Don't have an account yet? <a href="#" @click.prevent="openRegisterForm">Sign in now!</a></p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+
+  import '../assets/styles/LoginForm.css' 
+  export default {
+    name: 'LoginForm',
+    props: {
+      isOpen: {
+        type: Boolean,
+        required: true
+      }
+    },
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      submitForm() {
+        // Handle login logic here
+        console.log('Login submitted:', { username: this.username, password: this.password });
+        this.closeForm();
+      },
+      closeForm() {
+        this.$emit('close');
+      },
+      openRegisterForm() {
+        this.$emit('openRegister');
+        this.closeForm();
+      }
+    }
+  }
+  </script>
+  
