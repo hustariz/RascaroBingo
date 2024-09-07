56ce0174a150c788fd54a1d2f198aa8a10b00daf
Hastarus <Hugo.ustariz@gmail.com>
2024-09-07 21:51:35 +0200

    Reapply "Merge pull request #3 from hustariz/revert-1-restore-working-version"
    This reverts commit 8f1fd03c3fc0fba2bf896181fa13a90254870cd5.

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -5,11 +5,7 @@
     <div class="navbar-menu">
       <router-link to="/bingo" class="navbar-item">Bingo</router-link>
       <a href="#" class="navbar-item" @click.prevent="showContactForm">Contact</a>
-      <template v-if="isLoggedIn">
-        <span class="navbar-item">Welcome, {{ username }}</span>
-        <a href="#" class="navbar-item" @click.prevent="logout">Logout</a>
-      </template>
-      <a v-else href="#" class="navbar-item" @click.prevent="showLoginForm">Login</a>
+      <a href="#" class="navbar-item" @click.prevent="showLoginForm">Login</a>
     </div>
     <ContactForm :isOpen="isContactFormOpen" @close="closeContactForm" />
     <LoginForm :isOpen="isLoginFormOpen" @close="closeLoginForm" @openRegister="showRegisterForm" />
@@ -18,7 +14,6 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
 import '../assets/styles/AppNavbar.css';
 import ContactForm from './ContactForm.vue';
 import LoginForm from './LoginForm.vue';
@@ -31,12 +26,6 @@ export default {
     LoginForm,
     RegisterForm
   },
-  computed: {
-    ...mapState({
-      isLoggedIn: state => state.auth.isLoggedIn,
-      username: state => state.auth.user?.username
-    })
-  },
   data() {
     return {
       isContactFormOpen: false,
@@ -45,7 +34,6 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['logout']),
     showContactForm() {
       this.isContactFormOpen = true;
     },
