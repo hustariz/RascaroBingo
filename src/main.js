68e6c428ce680e8b94780b5074c428e972f148be
Hastarus <Hugo.ustariz@gmail.com>
2024-09-07 18:42:29 +0200

    Login Session, Frontend OK, TO DO: Backend gestion of login request

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './assets/styles/main.css';
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+store.dispatch('checkAuth')
+app.mount('#app')
