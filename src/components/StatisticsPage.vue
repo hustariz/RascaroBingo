30a8cec28b3f82432ab191e10b4294a21953bb72
Hastarus <Hugo.ustariz@gmail.com>
2024-09-09 16:59:20 +0200

    Added Profile and Stats page from user's session

---
+++ src/components/StatisticsPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="stats-page">
+      <h1>Statistics Page</h1>
+      <p>
+            Check your trading statistics here!
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  import '../assets/styles/HomePage.css';
+  
+  export default {
+    name: 'ProfilePage',
+    data() {
+      return {
+        welcomeMessage: 'Welcome to your stats summary'
+      }
+    }
+  }
+  </script>
+
+<style scoped>
+.stats-page {
+    margin-top: 60px; /* This matches the navbar height */
+}
+</style>
+
+  
