5b77e9c8b411b7cca6b4c07e78137b9f8163be34
Hastarus <Hugo.ustariz@gmail.com>
2024-09-09 16:59:20 +0200

    Added Profile and Stats page from user's session

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import BingoPage from '../components/BingoPage.vue'
+import ProfilePage from '@/components/ProfilePage.vue'
+import StatisticsPage from '@/components/StatisticsPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/bingo', component: BingoPage }
+  { path: '/bingo', component: BingoPage },
+  { path: '/profile', component: ProfilePage},
+  { path: '/statistics', component: StatisticsPage}
 ]
 
 const router = createRouter({
