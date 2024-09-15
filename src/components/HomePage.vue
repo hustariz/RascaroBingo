37efe3f96dc254072603530e3ff5aec425b92db4
Hastarus <Hugo.ustariz@gmail.com>
2024-09-15 15:15:37 +0200

    Tailwind CSS

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,12 +6,23 @@
        <br> while also providing <strong>stats</strong> of your trading sessions,
         <br> by providing a <strong>playful & stimulant frame</strong>! !
     </p>
-    <!--<div class="logo-container">
-      <router-link to="/bingo"> 
-        <img alt="Kakul logo" class="kakul-img" src="../assets/images/Mini_Kakul_Transp.png">
-        <span class="tooltip">Start Here</span>
-      </router-link>
-    </div> -->
+    <!--
+      <div class="bg-blue-500 text-white p-4">
+        Hello Tailwind CSS in Vue!
+      </div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      Testing Tailwind
+    --> 
+    <!--
+      <div class="logo-container">
+        <router-link to="/bingo"> 
+          <img alt="Kakul logo" class="kakul-img" src="../assets/images/Mini_Kakul_Transp.png">
+          <span class="tooltip">Start Here</span>
+        </router-link>
+    </div>
+    -->
   </div>
 </template>
 
