715d1d77d41fbd8190f3b4d67bf91156f2e25f15
Hastarus <Hugo.ustariz@gmail.com>
2024-12-12 15:10:38 +0100

    Putted back the css bridge for manual user target override

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore } from 'vuex'
 import bingo from './modules/bingo'
-import trades from './modules/trades';
-import riskManagement from './modules/riskManagement';
+import trades from './modules/trades'
+import riskManagement from './modules/riskManagement'
+import leaderboard from './modules/leaderboard'
+import shop from './modules/shop'
 
 export default createStore({
   modules: {
     bingo,
     trades,
-    riskManagement
+    riskManagement,
+    leaderboard,
+    shop
   }
 });
